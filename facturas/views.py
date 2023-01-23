499734896431b9627bf8a4f6fc378d2f5d608c50
Nelson C <nacasas8@misena.edu.co>
2023-01-23 10:51:19 -0500

    Fecha en modulo factura

--- facturas/views.py
+++ facturas/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from facturas.models import Detalle_Factura
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.views.generic import View
-from django.contrib.auth import login, logout, authenticate 
 from django.contrib import messages
 
+# Utilities
+from datetime import datetime
+
 def factura(request):
-    #return HttpResponse("Hola")
-    productos = Detalle_Factura.objects.all()
-    return render(request, "facturas/facturas.html", {'productos':Detalle_Factura})
+    """"Return a greeting. """
+    now = datetime.now().strftime('%m/%d/%Y, %H:%M:%S')
+    productos = Detalle_Factura.objects.all()        
+    return render(request, "facturas/facturas.html", {'now': now})
